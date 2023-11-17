e2abf1ab9553ceab9b565ee66bb786089dbee092
robertcrockett <robert.crockett@optum.com>
2023-11-17 09:30:13 -0500

    Setup initial testing framework

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/test/setup.ts"],
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
 });
