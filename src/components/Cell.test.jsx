a7df31004cacf7f365287e0726a7f2a525fe86d5
rcrocke2_uhg <robert.crockett@optum.com>
2023-11-25 08:50:48 -0500

    Set up more robust tests for the Cell component

--- src/components/Cell.test.jsx
+++ src/components/Cell.test.jsx
@@ -2,25 +2,86 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import Cell from "./Cell";
 
-function cellStatus() {
+function cellStatus_unselected() {
   return "unselected";
+}
+
+function cellStatus_challenge() {
+  return "blue";
+}
+
+function cellStatus_correct() {
+  return "correct";
+}
+
+function cellStatus_incorrect() {
+  return "incorrect";
 }
 
 function cellClick() {
   return "true";
 }
 
-describe("renders the Cell component", () => {
+describe("Renders the Cell component when unselected", () => {
   it("renders a cell", () => {
     render(
       <Cell
         cellValue={1}
-        cellStatus={() => cellStatus()}
+        cellStatus={() => cellStatus_unselected()}
         onClick={() => cellClick}
       />
     );
 
     const cellButton = screen.getByRole("button");
     expect(cellButton).toBeTypeOf("object");
+    expect(cellButton.style.backgroundColor).toBe("white");
+  });
+});
+
+describe("Renders the Cell component when it is part of challenge", () => {
+  it("renders a cell", () => {
+    render(
+      <Cell
+        cellValue={1}
+        cellStatus={() => cellStatus_challenge()}
+        onClick={() => cellClick}
+      />
+    );
+
+    const cellButton = screen.getByRole("button");
+    expect(cellButton).toBeTypeOf("object");
+    expect(cellButton.style.backgroundColor).toBe("lightblue");
+  });
+});
+
+describe("Renders the Cell component when correctly selected", () => {
+  it("renders a cell", () => {
+    render(
+      <Cell
+        cellValue={2}
+        cellStatus={() => cellStatus_correct()}
+        onClick={() => cellClick}
+      />
+    );
+
+    const cellButton = screen.getByRole("button");
+    expect(cellButton).toBeTypeOf("object");
+    expect(cellButton.style.backgroundColor).toBe("lightgreen");
+  });
+});
+
+describe("Renders the Cell component when incorrectly selected", () => {
+  it("renders a cell", () => {
+    render(
+      <Cell
+        cellValue={3}
+        cellStatus={() => cellStatus_incorrect()}
+        onClick={() => cellClick}
+      />
+    );
+
+    const cellButton = screen.getByRole("button");
+    expect(cellButton).toBeTypeOf("object");
+    expect(cellButton.style.backgroundColor).toBe("lightcoral");
   });
 });
