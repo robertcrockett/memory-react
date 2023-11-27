32d6736f55031424953808f683ad4725b405224e
rcrocke2_uhg <robert.crockett@optum.com>
2023-11-27 09:07:00 -0500

    Updated to latest versions

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,16 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "localhost",
+    port: 5173,
+  },
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: ["./src/test/setup.ts"],
   },
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
+  // esbuild: {
+  //   jsxInject: `import React from 'react'`,
+  // },
 });
