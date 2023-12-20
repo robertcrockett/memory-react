8879b792c4c31909b82e764eaa26ac26075340a6
rcrocke2_uhg <robert.crockett@optum.com>
2023-12-20 17:09:07 -0500

    Removed commented code

--- src/components/Game.test.jsx
+++ src/components/Game.test.jsx
@@ -21,9 +21,5 @@ describe("Renders the Game component", () => {
     // Test to validate the footer is rendered
     const footer = screen.getByTestId("footer");
     expect(footer).toBeTypeOf("object");
-
-    // Test to validate the cell is rendered
-    // const cell = screen.getByTestId("cell");
-    // expect(cell).toBeTypeOf("object");
   });
 });
