ce26e5d83d19716415207d3de8a41715144a76a8
robertcrockett <robertwcrockett@hotmail.com>
2025-01-04 10:10:30 -0500

    Typescript conversion.

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,17 +1,17 @@
 export const utils = {
   // Create an array of numbers between min and max (edges included)
-  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+  range: (min: number, max: number): number[] => Array.from({ length: max - min + 1 }, (_, i) => min + i),
 
   // Fisher-Yates Shuffle
-  shuffle: (array) => {
+  shuffle: (array: number[]): number[] => {
     let i = array.length,
-      temp;
+      temp: number;
 
     while (i--) {
       // Obtain a randomly chosen element
       temp = Math.floor(Math.random() * (i + 1));
 
-      // Swap randomly chosen element using deconstrution
+      // Swap randomly chosen element using deconstruction
       [array[temp], array[i]] = [array[i], array[temp]];
     }
 
